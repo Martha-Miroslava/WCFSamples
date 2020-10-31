a86a9f36e6608fb679568a2e70a62cf1a791c5d4
MARTHA <martha_15_7@outlook.com>
2020-10-30 22:40:26 -0600

    Agregar archivos de proyecto.

---
+++ TcpBinding.Server/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TcpBinding.Contracts;
+
+namespace TcpBinding.Server
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    class ProductService : IProductService
+    {
+        public string[] GetStrings()
+        {
+            return new[] { "server1", "server2", "server3" };
+        }
+    }
+}
